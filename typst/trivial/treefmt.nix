5ca5a415675cc450d9610de3add1b0d24422c6a9
Justin Hoang <j124.dev@proton.me>
2025-02-28 16:27:27 -0700

    fix: added treefmt.nix file for typst - trivial

---
+++ typst/trivial/treefmt.nix
@@ -0,0 +1,30 @@
+# treefmt.nix
+{ pkgs, ... }:
+{
+  # Used to find the project root
+  projectRootFile = "flake.nix";
+  settings = {
+    on-unmatched = "debug";
+  };
+
+  programs = {
+    # nix
+    nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
+    nixfmt.package = pkgs.nixfmt-rfc-style;
+
+    # typst
+    typstyle.enable = true;
+
+    # json
+    jsonfmt.enable = true;
+
+    # yaml
+    yamlfmt.enable = true;
+
+    # toml
+    taplo.enable = true;
+
+    # markdown
+    mdformat.enable = true;
+  };
+}
